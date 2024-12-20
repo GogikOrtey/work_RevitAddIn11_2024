b3a0be7969d92518471f7e516681c5c1c44afadc
Georg Orlov <orlov@altek.online>
2024-12-20 16:23:36 +0500

    Nuke сбилдил установщик

---
+++ source/Module2/ViewModels/Module2ViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Module2.ViewModels
+{
+    public sealed class Module2ViewModel : ObservableObject
+    {
+    }
+}
---
+++ source/Module2/Views/Module2View.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Module2.ViewModels;
+
+namespace Module2.Views
+{
+    public sealed partial class Module2View
+    {
+        public Module2View(Module2ViewModel viewModel)
+        {
+            DataContext = viewModel;
+            InitializeComponent();
+        }
+    }
+}
